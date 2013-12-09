1d9dcbe85c2fd24bdb77ad8b4adca08fc350ed0b
Martin Gorham <martin.gorham@sky.com>
2013-12-09 18:05:18 +0000

    Create cpp11_test.cpp

---
+++ cpp11_test.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <type_traits>
+
+template < typename T >
+void display( T&& t)
+{
+    if ( std::is_rvalue_reference<T&&>::value )
+    {
+        std::cout << t << " is an rvalue." << std::endl;
+    }
+    else
+    {
+        std::cout << t << " is an lvalue." << std::endl;
+    }
+}
+
+void process(void) {}
+
+template <typename Type, typename... Args>
+void process( Type&& type, Args&&... args )
+{
+    display( std::forward<Type&&>(type) );
+
+    process( std::forward<Args&&>(args)... );
+}
+
+int main(int argc, char* argv[] )
+{
+    std::string value ("martin");
+    const std::string test("test value");
+
+    size_t x = 42;
+
+    process(std::string("hello"), value.size(), x, true, value, "This is some text", std::move(test));
+
+    return 0;
+}
